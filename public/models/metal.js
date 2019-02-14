cf875adf3ace8e6f2332cb23f907358302660df9
Glitch (hello-express) <none>
2019-02-14 06:36:27 +0000

    🌟👝 Checkpoint ./views/index.html:3135861/424 ./.glitch-assets:3135861/390 ./public/models/metal.bin:3135861/12870 ./public/models/metal.js:3135861/508 ./public/models/metal.obj:3135861/10987

---
+++ public/models/metal.js
@@ -0,0 +1,20 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "sourceFile"    : "/models/metal.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 97,
+        "faces"         : 101,
+        "normals"       : 193,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}]
+}
