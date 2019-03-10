6d7ef35d1797c55d1bc387b77e1dbb47235aa879
Glitch (lathejs) <none>
2019-03-10 00:34:52 +0000

    🐋🌎 Checkpoint ./public/main.js:3135861/2

--- public/main.js
+++ public/main.js
@@ -355,7 +355,7 @@ function initObjects() {
   lathe.geometry.computeFaceNormals();
   lathe.geometry.computeVertexNormals();
   
-  lathe.position.z = -0.5 - lathe.radius; //position the lathe a little bit in front of the screen
+  //lathe.position.z = -0.5 - lathe.radius; //position the lathe a little bit in front of the screen
   //lathe.position.x = - 0.5 * lathe.totalLinks * lathe.linkDist //center it horizontally 
   //lathe.rotation.y = 90 * TO_RADIANS; 
   
