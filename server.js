9012fd853b010bc891bb1e17f65c3dd2491ba0e9
vedants <vedantsaran@berkeley.edu>
2019-03-15 17:53:19 -0700

    cleaned up server

--- server.js
+++ server.js
@@ -1,41 +1,21 @@
-// where the node app starts
-
-// init project
 var express = require('express');
-var fs = require('fs');   //filesystem 
-var ws = require('ws')  //websocket
+var fs = require('fs');
+var ws = require('ws');
 var app = express();
 var bodyParser = require('body-parser');
 var longpoll = require("express-longpoll")(app, { DEBUG: false });
-
 var WebSocketServer = require('ws').Server;
+
+app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', parameterLimit: 100000, extended: true }));
+
+
 var wss = new WebSocketServer({port: 40510});
 var connections = []; 
 
-// wsServer.on('request', function(request) {
-//   var connection = request.accept('echo-protocol', request.origin);
-//   connections.push(connection);
-//   console.log((new Date()) + ' Connection accepted.');
-  
-//   connection.on('message', function(message) {
-//       if (message.type === 'utf8') {
-//           console.log('Received Message: ' + message.utf8Data);
-//         //send the received message to all of the connections in the connection array
-//         for(var i = 0; i < connections.length; i++) {
-//             connections[i].sendUTF(message.utf8Data);
-//         }
-//       }
-//   });
-//   connection.on('close', function(reasonCode, description) {
-//     console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-//   });
-// });
-
-
-
 wss.on('connection', function (ws) {
-  //connections.push(connection);
-  var connection = ws;//.accept('echo-protocol', ws.origin);
+  var connection = ws;
   connections.push(connection);
 
   ws.on('message', function (message) {
@@ -46,13 +26,6 @@ wss.on('connection', function (ws) {
     }
   });
 });
-
-
-var lathe_json_data = {}; 
-
-app.use(express.static('public'));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', parameterLimit: 100000, extended: true }));
 
 var is_changed; 
 var segmentFactors = []; //stores the server view of the segmentFactors
@@ -99,41 +72,10 @@ app.post('/cut', function(request, response) {
   response.send("got cut.");
 });
 
-
-
-app.get('/update_lathe', function(request, response) {
-  console.log("updating lathe status...");
-  longpoll.publish("/is_lathe_updated", "true");
-  response.send("updated");
-  console.log("updated lathe status.");
-});
-
-app.get('load_lathe', function(reuqest, response) {
-  console.log("loading lathe..."); 
-  var x = lathe_json_data.slice(0);
-  response.send(x);
-});
-
-
 app.post('/save_lathe', function(request, response) {
   console.log("saving lathe @" + new Date().toLocaleTimeString());
   lathe_json_data = request.body; 
   response.send("closing connection."); 
-  
-  /*
-  console.log("request"); 
-  console.log(request.body);
-  var suffix = Math.random();
-  fs.writeFile(__dirname + "/public/models/lathe_model" + suffix + ".js", JSON.stringify(request.body), (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    };
-    console.log("File has been created");
-  });
-  response.send("file has been created.");   
-  console.log(new Date().getTime() / 1000);
-  */
 });
 
 //checks if the lathe has been cut by the hand wheels. 
